8dc8dd1a7558761b24207130b8cd83da6da14fb7
Curtis Myzie <curtis.myzie@gmail.com>
2022-11-03 12:06:14 -0400

    VSCode plugin

---
+++ vscode/LINKS.md
@@ -0,0 +1,13 @@
+## Extensions
+
+https://code.visualstudio.com/api/working-with-extensions/publishing-extension
+
+https://code.visualstudio.com/api/language-extensions/language-server-extension-guide#implementing-a-language-server
+
+https://github.com/TobiasYin/go-lsp
+
+https://github.com/microsoft/vscode-extension-samples/tree/main/helloworld-sample
+
+https://github.com/jdbaldry/go-language-server-protocol
+
+https://github.com/sourcegraph/go-langserver
---
+++ vscode/client/src/extension.ts
@@ -0,0 +1,69 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	// The server is implemented in node
+	// const serverModule = context.asAbsolutePath('/Users/curtis/git/tamarin/cmd/lsp');
+	// path.join('server', 'out', 'server.js')
+	// );
+	// The debug options for the server
+	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	const serverOptions: ServerOptions = {
+		run: {
+			command: '/Users/curtis/git/tamarin/cmd/tamarin-lsp/tamarin-lsp',
+			args: [],
+			options: {},
+		},
+		debug: {
+			command: '/Users/curtis/git/tamarin/cmd/tamarin-lsp/tamarin-lsp',
+			args: [],
+			options: {},
+		}
+	};
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'languageServerExample',
+		'Language Server Example',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
