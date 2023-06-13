3cbe1f470f472437cccde73942da8b1c8d802370
Curtis Myzie <curtis.myzie@gmail.com>
2023-06-13 12:24:54 -0400

    Adjust deps

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/jdbaldry/go-language-server-protocol v0.0.0-20211013214444-3022da0884b2
 	github.com/rs/zerolog v1.28.0
 	github.com/stretchr/testify v1.8.0
-	github.com/wI2L/jsondiff v0.3.0
+	github.com/wI2L/jsondiff v0.4.0
 )
 
 require (
@@ -26,9 +26,6 @@ require (
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
-	github.com/tidwall/gjson v1.14.3 // indirect
-	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	golang.org/x/text v0.3.8 // indirect
--- vm/run.go
+++ vm/run.go
@@ -6,7 +6,14 @@ import (
 	"github.com/cloudcmds/tamarin/v2/builtins"
 	"github.com/cloudcmds/tamarin/v2/compiler"
 	"github.com/cloudcmds/tamarin/v2/importer"
-	"github.com/cloudcmds/tamarin/v2/modules/all"
+	modBytes "github.com/cloudcmds/tamarin/v2/modules/bytes"
+	modJson "github.com/cloudcmds/tamarin/v2/modules/json"
+	modMath "github.com/cloudcmds/tamarin/v2/modules/math"
+	"github.com/cloudcmds/tamarin/v2/modules/os"
+	modRand "github.com/cloudcmds/tamarin/v2/modules/rand"
+	modStrconv "github.com/cloudcmds/tamarin/v2/modules/strconv"
+	modStrings "github.com/cloudcmds/tamarin/v2/modules/strings"
+	modTime "github.com/cloudcmds/tamarin/v2/modules/time"
 	"github.com/cloudcmds/tamarin/v2/object"
 	"github.com/cloudcmds/tamarin/v2/parser"
 )
@@ -14,7 +21,10 @@ import (
 func run(ctx context.Context, code string) (object.Object, error) {
 
 	builtins := builtins.Defaults()
-	for k, v := range all.Defaults() {
+	for k, v := range os.Builtins() {
+		builtins[k] = v
+	}
+	for k, v := range defaultModules() {
 		builtins[k] = v
 	}
 
@@ -45,4 +55,16 @@ func run(ctx context.Context, code string) (object.Object, error) {
 		return result, nil
 	}
 	return object.Nil, nil
+}
+
+func defaultModules() map[string]object.Object {
+	return map[string]object.Object{
+		"math":    modMath.Module(),
+		"json":    modJson.Module(),
+		"strings": modStrings.Module(),
+		"time":    modTime.Module(),
+		"rand":    modRand.Module(),
+		"strconv": modStrconv.Module(),
+		"bytes":   modBytes.Module(),
+	}
 }
