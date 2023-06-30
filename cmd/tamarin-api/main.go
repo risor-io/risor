457c0e93165e4b8dabe03b2d5cca264ba34a3f4e
towhid <ktowhid20@gmail.com>
2023-07-01 00:16:31 +0600

    initial api creation finished

--- cmd/tamarin-api/main.go
+++ cmd/tamarin-api/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/cloudcmds/tamarin/v2"
+	"github.com/cloudcmds/tamarin/v2/modules/all"
 	"github.com/cloudcmds/tamarin/v2/parser"
 
 	"github.com/go-chi/chi/v5"
@@ -16,39 +17,47 @@ import (
 )
 
 type Request struct {
-    ID      int    `json: "id"`
-    Content string `json: "content"`
+	Content string `json:"content"`
+}
+
+type Response struct {
+	Result string `json:"result"`
+	Time   float64 `json:"time"`
 }
 
 func main() {
-	r := chi.NewRouter()
-    r.Use(middleware.Logger)
-	r.Post("/execute", func(w http.ResponseWriter, r *http.Request) {
-        executeHandler(w, r)
-    })
+	var port string
+	flag.StringVar(&port, "port", "3000", "Define port for the server to listen on")
+	flag.Parse()
 
-	log.Println("Server started on http://localhost:3000")
-	log.Fatal(http.ListenAndServe(":3000", r))
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/execute", func(w http.ResponseWriter, r *http.Request) {
+		executeHandler(w, r)
+	})
+
+	log.Println("Server started on http://localhost:" + port)
+	log.Fatal(http.ListenAndServe(":" + port, r))
 }
 
 func executeHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse request body
-	var req Request 
-    json.NewDecoder(r.Body).Decode(&req)
-
-	fmt.Println("qwe",req)
-
-	// if err != nil {
-	// 	http.Error(w, "Failed to parse request body", http.StatusBadRequest)
-	// 	return
-	// }
+	var req Request
+	var res Response
+	json.NewDecoder(r.Body).Decode(&req)
 
 	input := req.Content
+	if input == "" {
+		http.Error(w, "Please provide a code snippet", http.StatusBadRequest)
+		return
+	}
 
 	ctx := context.Background()
 	start := time.Now()
 
-	result, err := tamarin.Eval(ctx, string(input))
+	result, err := tamarin.Eval(ctx, string(input),
+		tamarin.WithBuiltins(all.Builtins()),
+		tamarin.WithDefaultBuiltins(),
+		tamarin.WithDefaultModules())
 	if err != nil {
 		parserErr, ok := err.(parser.ParserError)
 		if ok {
@@ -59,55 +68,17 @@ func executeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response := result.Inspect()
-	
-	response += fmt.Sprintf("\n%.03f", time.Since(start).Seconds())
+	res.Result = result.Inspect()
 
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprint(w, response)
+	res.Time = time.Since(start).Seconds()
+
+	response, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(response)
 }
-
-
-
-
-// package main
-
-// import (
-// 	"context"
-// 	"fmt"
-// 	"os"
-// 	"time"
-
-// 	"github.com/cloudcmds/tamarin/v2"
-// 	"github.com/cloudcmds/tamarin/v2/object"
-// 	"github.com/cloudcmds/tamarin/v2/parser"
-// )
-
-// func main() {
-// 	ctx := context.Background()
-// 	start := time.Now()
-
-// 	// data to get
-// 	showTiming := true
-// 	// input := []byte(`print("sentence")`)
-
-// 	result, err := tamarin.Eval(ctx,
-// 		`print("sentence")`,
-// 	)
-// 	if err != nil {
-// 		parserErr, ok := err.(parser.ParserError)
-// 		if ok {
-// 			fmt.Fprintf(os.Stderr, "%s\n", parserErr.FriendlyMessage())
-// 		} else {
-// 			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-// 		}
-// 		os.Exit(1)
-// 	}
-// 	if result != object.Nil {
-// 		fmt.Println(result.Inspect())
-// 	}
-// 	if showTiming {
-// 		fmt.Printf("%.03f\n", time.Since(start).Seconds())
-// 	}
-
-// }
