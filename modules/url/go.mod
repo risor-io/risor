3d64820fbe843773dc0a0b7dd6eb4e9e3fd2996f
Sergio Rubio <rubiojr@rbel.co>
2024-09-09 19:20:04 +0200

    Add basic URL module

---
+++ modules/url/go.mod
@@ -0,0 +1,9 @@
+module github.com/risor-io/risor/modules/uuid
+
+go 1.22
+
+toolchain go1.22.0
+
+replace github.com/risor-io/risor => ../..
+
+require github.com/risor-io/risor v1.6.0
---
+++ modules/url/url.go
@@ -0,0 +1,28 @@
+package uuid
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/risor-io/risor/internal/arg"
+	"github.com/risor-io/risor/object"
+)
+
+func QueryEscape(ctx context.Context, args ...object.Object) object.Object {
+	if err := arg.Require("query_escape", 1, args); err != nil {
+		return err
+	}
+	q, err := object.AsString(args[0])
+	if err != nil {
+		return err
+	}
+
+	s := url.QueryEscape(q)
+	return object.NewString(s)
+}
+
+func Module() *object.Module {
+	return object.NewBuiltinsModule("url", map[string]object.Object{
+		"query_escape": object.NewBuiltin("v7", QueryEscape),
+	}, QueryEscape)
+}
