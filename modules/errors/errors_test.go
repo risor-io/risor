ed2e9f0aa3ff586285adb867d78f771c0637e640
Curtis Myzie <curtis.myzie@gmail.com>
2024-09-26 16:39:31 -0400

    errors module improvements

--- modules/errors/errors_test.go
+++ modules/errors/errors_test.go
@@ -2,8 +2,10 @@ package errors
 
 import (
 	"context"
+	"errors"
 	"testing"
 
+	"github.com/risor-io/risor/errz"
 	"github.com/risor-io/risor/object"
 	"github.com/stretchr/testify/require"
 )
@@ -21,11 +23,44 @@ func TestErrors(t *testing.T) {
 	require.Equal(t, "error \"foo bar\" 42", errObj.Value().Error())
 }
 
-func TestBadErrorsCall(t *testing.T) {
+func TestEmptyError(t *testing.T) {
 	e := New(context.Background())
 	require.IsType(t, &object.Error{}, e)
 	errObj, ok := e.(*object.Error)
 	require.True(t, ok)
-	require.True(t, errObj.IsRaised())
-	require.Equal(t, "type error: errors.new() takes 1 or more arguments (0 given)", errObj.Value().Error())
+	require.False(t, errObj.IsRaised())
+	require.Equal(t, "", errObj.Value().Error())
+}
+
+func TestErrorTypes(t *testing.T) {
+	e1 := EvalError(context.Background(), object.NewString("e1")).(*object.Error)
+	e2 := TypeError(context.Background(), object.NewString("e2")).(*object.Error)
+	e3 := ArgsError(context.Background(), object.NewString("e3")).(*object.Error)
+
+	var evalErr *errz.EvalError
+	var typeErr *errz.TypeError
+	var argsErr *errz.ArgsError
+
+	require.True(t, errors.As(e1.Value(), &evalErr))
+	require.False(t, errors.As(e1.Value(), &typeErr))
+	require.False(t, errors.As(e1.Value(), &argsErr))
+
+	require.False(t, errors.As(e2.Value(), &evalErr))
+	require.True(t, errors.As(e2.Value(), &typeErr))
+	require.False(t, errors.As(e2.Value(), &argsErr))
+
+	require.False(t, errors.As(e3.Value(), &evalErr))
+	require.False(t, errors.As(e3.Value(), &typeErr))
+	require.True(t, errors.As(e3.Value(), &argsErr))
+}
+
+func TestErrorsAs(t *testing.T) {
+	e1 := EvalError(context.Background(), object.NewString("e1")).(*object.Error)
+	evalErr := EvalError(context.Background()).(*object.Error)
+	typeErr := TypeError(context.Background()).(*object.Error)
+	genericErr := New(context.Background()).(*object.Error)
+
+	require.Equal(t, object.True, As(context.Background(), e1, evalErr))
+	require.Equal(t, object.False, As(context.Background(), e1, typeErr))
+	require.Equal(t, object.True, As(context.Background(), e1, genericErr))
 }
