29e04063eea28ae1e3d629cf11215aa28ad3510b
Sergio Rubio <rubiojr@rbel.co>
2025-01-19 18:01:24 +0100

    Address @dnbsd feedback

--- object/time_test.go
+++ object/time_test.go
@@ -6,42 +6,36 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTimeAddDate(t *testing.T) {
 	baseTime := NewTime(time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC))
 
 	tests := []struct {
-		years   int64
-		months  int64
-		days    int64
-		want    time.Time
-		wantErr bool
+		years  int64
+		months int64
+		days   int64
+		want   time.Time
 	}{
-		{1, 0, 0, time.Date(2024, 10, 1, 0, 0, 0, 0, time.UTC), false},
-		{0, 1, 0, time.Date(2023, 11, 1, 0, 0, 0, 0, time.UTC), false},
-		{0, 0, 1, time.Date(2023, 10, 2, 0, 0, 0, 0, time.UTC), false},
-		{1, 1, 1, time.Date(2024, 11, 2, 0, 0, 0, 0, time.UTC), false},
-		{-1, 0, 0, time.Date(2022, 10, 1, 0, 0, 0, 0, time.UTC), false},
-		{-1, 0, -1, time.Date(2022, 9, 30, 0, 0, 0, 0, time.UTC), false},
-		{0, 0, 0, time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC), false},
+		{1, 0, 0, time.Date(2024, 10, 1, 0, 0, 0, 0, time.UTC)},
+		{0, 1, 0, time.Date(2023, 11, 1, 0, 0, 0, 0, time.UTC)},
+		{0, 0, 1, time.Date(2023, 10, 2, 0, 0, 0, 0, time.UTC)},
+		{1, 1, 1, time.Date(2024, 11, 2, 0, 0, 0, 0, time.UTC)},
+		{-1, 0, 0, time.Date(2022, 10, 1, 0, 0, 0, 0, time.UTC)},
+		{-1, 0, -1, time.Date(2022, 9, 30, 0, 0, 0, 0, time.UTC)},
+		{0, 0, 0, time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC)},
+		{0, 0, 0, time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC)},
 	}
 
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("AddDate(%d, %d, %d)", tt.years, tt.months, tt.days), func(t *testing.T) {
 			result := baseTime.AddDate(context.Background(), NewInt(tt.years), NewInt(tt.months), NewInt(tt.days))
 
-			if tt.wantErr {
-				_, ok := result.(*Error)
-				assert.True(t, ok, "expected error, got %v", result)
-				return
-			}
-
-			assert.Equal(t, TIME, result.Type(), "expected TIME, got %v", result.Type())
+			require.Equal(t, TIME, result.Type(), "expected TIME, got %v", result.Type())
 
 			resultTime := result.(*Time).Value()
-			assert.True(t, resultTime.Equal(tt.want), "expected %v, got %v", tt.want, resultTime)
+			require.Equal(t, tt.want, resultTime)
 		})
 	}
 }
